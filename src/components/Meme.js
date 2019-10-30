9d7ad16bc60ae428e25610b46964fa4e61dce7f5
Samuel Boateng <kaybeta500@gmail.com>
2019-10-30 00:18:21 +0000

    update: meme  component styling and data design

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,26 +3,29 @@ import React from 'react'
 import './Meme.css'
 
 const Meme = props => {
-    const { scrollUp, scrollDown, memeEntry } = props
+    const { indexOfVisible, memes } = props
+    const selected = memes[indexOfVisible]
+    
     return (
         <div className="ui row grid stackable meme">
-            <section className="flex-row">
-                <div className="memesCount border flex-column">
-                <div onClick={scrollUp} id="up" className="caret up-caret"></div>
-                    <p>{memeEntry.index}</p>
-                <div onClick={scrollDown} className="caret down-caret"></div>
+            <section id="meme-left" className="border flex-row">
+                <div className="item memesCount border flex-column">
+                    <div id="up" className="caret up-caret"></div>
+                        <p>{indexOfVisible + 1}</p>
+                    <div  className="caret down-caret"></div>
                 </div>
-                <div className="meme-img">
-                    <img src={memeEntry.img} />
+                <div className="item meme-img">
+                    <img src={selected.src} />
                 </div>
             </section>
             
             <div className="meme-details">
-                <h2 className="ui header meme-title ">{memeEntry.title}</h2>
-                <p className="ui lead meme-comment-count">{memeEntry.commentsCount} comments</p>
+                <h2 className="ui header meme-title">{selected.title}</h2>
+                <p className="ui lead meme-comment-count">{selected.comments.length} comments</p>
                 <div className="meme-author">
-                    <div className="circle"></div>
-                    <p> Kofi Babone</p>
+                    
+                        <img  className="circle" src={selected.author.img} />
+                        <p>{selected.timestamp}</p>
                 </div>
             </div>
         </div>
