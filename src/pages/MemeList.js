3e5a1eff9c9cc6dd29c6f53b81da754a56d03610
Samuel Boateng <kaybeta500@gmail.com>
2019-10-30 00:20:21 +0000

    memelist component

--- src/pages/MemeList.js
+++ src/pages/MemeList.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Meme from '../components/Meme.js'
+import Modal from '../components/Modal.js'
+
+import { loadMemes } from  '../reducers/memeActions'
 
 class MemeList extends Component {
-    renderMemes() {
-        const memeEntry = {
-            title: 'What is a meme?',
-            commentsCount: '1200',
-            img: 'meme.jpg',
-            index: '256'
-        }
-        return <Meme 
-            scrollUp={() => console.log('increment')}
-            scrollDown={() => console.log('decrement')}
-            memeEntry={memeEntry}/>
+    componentDidMount() {
+        if(!this.props.isLoggedIn) this.props.history.push('/')
+        else this.props.loadMemes()
     }
-    
-    render(){
-        return(
+
+    render() {
+        const { memesList, currentIndex, error } = this.props
+        console.log(this.props.memesList)
+        return (    
             <div className="ui container fluid grid stackable">
+                <Modal>  
+                    <div className="ui header">Header</div>
+                    <div className="content">
+                        content
+                    </div>            
+                </Modal>
                 <div id="top" className="ui row border">
                     <div id="logo" className="ui circle left aligned">MF</div>
                     <div className="ui circle right aligned">MF</div>
@@ -30,17 +33,25 @@ class MemeList extends Component {
                             <button id="upload-btn" className="ui button">Upload New Meme</button>
                         </div>
                         <div id="content" className="ui border row">
-                            { this.renderMemes() }
+                            {  this.props.memesList.length > 0 
+                                && <Meme memes={memesList} 
+                                        indexOfVisible={currentIndex} /> }
+                            
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div>   
+          
         )
     }
 }
 
-const mapStateToProps = () => {
-    return {}
+const mapStateToProps = ({memes, login}) => {
+    const { memesList, currentIndex, error } = memes
+    const { isLoggedIn } = login
+    return { memesList, currentIndex, error, isLoggedIn }
 }
 
-export default connect(null)(MemeList)
+
+export default connect(mapStateToProps,
+    { loadMemes })(MemeList)
